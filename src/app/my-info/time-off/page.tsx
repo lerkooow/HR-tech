cbd939ff34eba4ebd34f15d409fcdc9a92206597
Valeriia Akhmatova <lerkoooow@gmail.com>
2024-09-19 23:36:32 +0600

    fix page my info/time-off

--- src/app/my-info/time-off/page.tsx
+++ src/app/my-info/time-off/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     const token = localStorage.getItem("access_token");
 
     if (!token) {
-      router.push("/login");
+      router.push("/");
     } else {
       setIsCheckingToken(false);
     }
